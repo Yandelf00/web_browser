e6812c1d52409cc8379a1336c054b2631c4ebc23
Yandelf00 <125671903+Yandelf00@users.noreply.github.com>
2024-06-26 21:24:34 +0100

    implemented core features of browser

---
+++ browzie.py
@@ -0,0 +1,114 @@
+import socket
+import ssl
+
+class URL : 
+    def __init__(self, url):
+        self.scheme, url = url.split("://", 1)
+        assert self.scheme in ["http", "https"] 
+        if self.scheme == "http": 
+            self.port = 80
+        elif self.scheme == "https": 
+            self.port = 443
+        if '/' not in url : 
+            url = url + '/'
+        self.host, url = url.split('/', 1)
+        self.path = '/' + url
+        if ":" in self.host : 
+            self.host, port = self.host.split(":", 1)
+            self.port = int(port)
+        
+
+    def request(self) : 
+        s = socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+            proto= socket.IPPROTO_TCP
+        )
+        s.connect((self.host, self.port))
+        if self.scheme == "https":
+            ctx = ssl.create_default_context()
+            s = ctx.wrap_socket(s, server_hostname=self.host)
+        request = f"GET {self.path} HTTP/1.0\r\n"
+        request += f"Host: {self.host}\r\n"
+        request += "\r\n"
+        s.send(request.encode('utf8'))
+        response = s.makefile("r", encoding="utf8", newline="\r\n")
+        statusline = response.readline()
+        version, status, explanation = statusline.split(" ", 2)
+        response_headers = {}
+        while True : 
+            line = response.readline()
+            if line == "\r\n" : break
+            header, value = line.split(':', 1)
+            response_headers[header.casefold()] = value.strip()
+
+        assert "transfer-encoding" not in response_headers
+        assert "content-encoding" not in response_headers
+        content = response.read()
+        s.close()
+
+        return content
+
+
+def show(body): 
+    in_tag = False
+    for c in body: 
+        if c == "<" :
+            in_tag = True
+        elif c == ">" :
+            in_tag = False
+        elif not in_tag : 
+            print(c, end="")
+
+
+def load(url):
+    body = url.request()
+    show(body)
+
+if __name__ == "__main__":
+    import sys
+    load(URL(sys.argv[1]))
+
+
+
+"""
+    form of the URL : 
+    http://example.org/index.html
+    http = Scheme (how to get the into)
+    example.org = Hostname (where to get the info)
+    /index.html = path (what info to get)
+
+___________________________________________________
+
+    form of a GET request : 
+    GET /index.html HTTP/1.0
+    Host: example.org
+
+    GET = method
+    /index.html = path
+    HTTP/1.0 = http version 
+    Host = header 
+    example.org = value
+
+_____________________________________________________
+
+    form of a server response : 
+    HTTP/1.0 200 OK 
+
+    Age: 545933
+    Cache-Control: max-age=604800
+    Content-Type: text/html; charset=UTF-8
+    Date: Mon, 25 Feb 2019 16:49:28 GMT
+    Etag: "1541025663+gzip+ident"
+    Expires: Mon, 04 Mar 2019 16:49:28 GMT
+    Last-Modified: Fri, 09 Aug 2013 23:54:35 GMT
+    Server: ECS (sec/96EC)
+    Vary: Accept-Encoding
+    X-Cache: HIT
+    Content-Length: 1270
+    Connection: close
+
+    HTTP/1.0 = HTTP version
+    200 = response code
+    OK = response description
+"""
